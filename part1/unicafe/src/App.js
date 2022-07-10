0d5fdfdfbaf398a957a92911ded88ddec215793f
Alexander Zech <alexzech@gmx.de>
2022-07-09 21:04:25 -0700

    1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,6 +12,20 @@ const Section = ({title}) => <h1>{title}</h1>;
 
 const Result = ({text, result}) => <div>{text} {result}</div>
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <Section title={"statistics"} />
+      <Result text={"good"} result={good} />
+      <Result text={"neutral"} result={neutral} />
+      <Result text={"bad"} result={bad} />
+      <Result text={"all"} result={good + neutral + bad} />
+      <Result text={"average"} result={averageScore({good, neutral, bad})} />
+      <Result text={"positive"} result={percentagePositive({good, neutral, bad})} />
+    </div>
+  )
+}
+
 function averageScore({good, neutral, bad}) {
   const sum = good + neutral + bad;
   return (good - bad)/sum;
@@ -44,13 +58,7 @@ const App = () => {
         text={"bad"}
       />
 
-      <Section title={"statistics"} />
-      <Result text={"good"} result={good} />
-      <Result text={"neutral"} result={neutral} />
-      <Result text={"bad"} result={bad} />
-      <Result text={"all"} result={good + neutral + bad} />
-      <Result text={"average"} result={averageScore({good, neutral, bad})} />
-      <Result text={"positive"} result={percentagePositive({good, neutral, bad})} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
