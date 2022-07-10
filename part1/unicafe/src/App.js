709be196719dfc5c132db989f2b0854029f684eb
Alexander Zech <alexzech@gmx.de>
2022-07-09 20:56:14 -0700

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,7 +10,17 @@ const Button = (props) => {
 
 const Section = ({title}) => <h1>{title}</h1>;
 
-const Result = ({text, result}) => <div>{text}: {result}</div>
+const Result = ({text, result}) => <div>{text} {result}</div>
+
+function averageScore({good, neutral, bad}) {
+  const sum = good + neutral + bad;
+  return (good - bad)/sum;
+}
+
+function percentagePositive({good, neutral, bad}) {
+  const sum = good + neutral + bad;
+  return `${100 * good/sum} %`;
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -38,6 +48,9 @@ const App = () => {
       <Result text={"good"} result={good} />
       <Result text={"neutral"} result={neutral} />
       <Result text={"bad"} result={bad} />
+      <Result text={"all"} result={good + neutral + bad} />
+      <Result text={"average"} result={averageScore({good, neutral, bad})} />
+      <Result text={"positive"} result={percentagePositive({good, neutral, bad})} />
     </div>
   )
 }
