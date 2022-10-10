2801cd67596cb32055429ef4e44f2defea220256
Alexander Zech <alexzech@gmx.de>
2022-10-09 21:32:24 -0700

    4.8: Blog list tests, step1

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT // eslint-disable-line no-undef
-const MONGODB_URI = process.env.MONGODB_URI // eslint-disable-line no-undef
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
