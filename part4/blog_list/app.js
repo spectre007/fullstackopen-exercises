24a735c0cf6e0e026bebbda28e5ee5c2d06c0d26
Alexander Zech <alexzech@gmx.de>
2022-10-23 21:26:42 -0700

    4.15: bloglist expansion, step3

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/blog_list/tests/test_helper.js
+++ part4/blog_list/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -57,6 +58,11 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((user) => user.toJSON())
+}
+
 module.exports = {
-  initialBlogs, blogsInDb, nonExistingId
+  initialBlogs, blogsInDb, nonExistingId, usersInDb
 }
