48e808bf034f49b6c2b94e60672c4f5ecc4e62cd
Alexander Zech <alexzech@gmx.de>
2022-10-26 20:33:03 -0700

    4.18: bloglist expansion, step6

---
+++ part4/blog_list/tests/login_api.test.js
@@ -0,0 +1,49 @@
+const supertest = require('supertest')
+const bcrypt = require('bcrypt')
+const helper = require('./test_helper')
+const app = require('../app')
+const User = require('../models/user')
+
+const api = supertest(app)
+
+describe('login for user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('geheim', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('succeeds with correct password and returns token', async () => {
+    const loginDetails = {
+      username: 'root',
+      password: 'geheim',
+    }
+
+    result = await api
+      .post('/api/login')
+      .send(loginDetails)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body).toHaveProperty('token')
+    expect(result.body).toHaveProperty('username')
+  })
+
+  test('fails with statuscode if password is wrong', async () => {
+    const loginDetails = {
+      username: 'root',
+      password: 'wrongPassword',
+    }
+
+    result = await api
+      .post('/api/login')
+      .send(loginDetails)
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('invalid username or password')
+  })
+})
