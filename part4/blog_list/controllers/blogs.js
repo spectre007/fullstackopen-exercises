d3fcc7fbc9506b25f13736e3a1410bdfbe0336b2
Alexander Zech <alexzech@gmx.de>
2022-11-05 13:33:12 -0700

    4.21: bloglist expansion, step9

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -55,8 +55,24 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).json({ error: 'blog not found' })
+  }
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!(decodedToken.id)) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  const { _id: userid } = user
+
+  if ( blog.user.toString() === userid.toString() ) {
+    await blog.remove()
+    response.status(204).end()
+  } else {
+    response.status(401).json({ error: 'unauthorized user' })
+  }
 })
 
 module.exports = blogsRouter
--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -2,8 +2,12 @@
 const logger = require('./logger')
 
 const errorHandler = (error, request, response, next) => {
-  if (error.name === 'ValidationError') {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
   }
 
   next(error)
@@ -12,7 +16,7 @@ const errorHandler = (error, request, response, next) => {
 const tokenExtractor = (request, response, next) => {
   const pattern = /^(bearer|Bearer)\s+([A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*)$/
   const authorization = request.get('authorization')
-  
+
   if (authorization && pattern.test(authorization)) {
     request.token = authorization.match(pattern)[2]
   }
