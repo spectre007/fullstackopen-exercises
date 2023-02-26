9cf8f3b1b2dcd6e33f905fed3d880e356056aeaf
Alexander Zech <alexzech@gmx.de>
2023-02-25 22:43:06 -0800

    5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ part5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const testUser = {
+      name: 'Rumpertus',
+      username: 'rumppi',
+      password: 'let-me-in',
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, testUser)
     cy.visit('')
   })
 
@@ -8,5 +14,27 @@ describe('Blog app', function() {
     cy.contains('Log in to application')
     cy.get('#username')
     cy.get('#password')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('rumppi')
+      cy.get('#password').type('let-me-in')
+      cy.get('#login-button').click()
+
+      cy.contains('Rumpertus logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('rumppi')
+      cy.get('#password').type('wrong-password')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.contains('Rumpertus logged in').should('not.exist')
+    })
   })
 })
