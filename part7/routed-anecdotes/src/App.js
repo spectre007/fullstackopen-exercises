dae76f48fbdb67933f6285b3cac5ed265fc71481
Alexander Zech <alexzech@gmx.de>
2023-10-11 22:42:10 -0700

    7.5: anecdotes and hooks step2 (incl 7.6)

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -75,9 +75,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
 
 
   const handleSubmit = (e) => {
@@ -88,6 +88,13 @@ const CreateNew = (props) => {
         info: info.value,
         votes: 0
     })
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
@@ -107,6 +114,7 @@ const CreateNew = (props) => {
           <input name='info' { ...info } />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
